e43798fcf4ab6859124b402d6dac6f71fee85bee
Kobi Cummings <kobicummings@gmail.com>
2019-10-21 19:33:43 -0700

    added heroku variables

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
-
+var PORT = process.env.PORT || 3000;
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/unit18Populater";
 // Initialize Express
 var app = express();
 
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true
 });
 
